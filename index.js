09fb431e0bc029c373f3cf99b5e134e6247b7236
Reuben Remy <reubenremy@gmail.com>
2020-04-25 22:15:56 -0400

    Small syntax update

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 
 start()
 
-get(prompts, (err, result) => {
+get(prompts, (err, results) => {
     createTransport({
         service: 'gmail',
         auth: {
             user: process.env.EMAIL,
             pass: process.env.PASS
         }
-    }).sendMail(result, (err, info)=> {
+    }).sendMail(results, (err, info)=> {
         err ? console.log(err) :
         console.log('Email Sent!', info);
     })
